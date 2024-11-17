35cad8d9616a46d76873d79ab4620b0cc0e7b257
Sergei <vavilin.dev@gmail.com>
2024-11-17 22:37:18 +0300

    created buyer and game models

--- Gamestore/task1/models.py
+++ Gamestore/task1/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Buyer(models.Model):
-    name = models.CharField(max_length=30, required=True)
-    balance = models.DecimalField()
+    name = models.CharField(max_length=30)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
     age = models.IntegerField()
 
 
 class Game (models.Model):
     title = models.CharField(max_length=30)
-    cost = models.DecimalField()
-    size = models.DecimalField()
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     age_limited  = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='game')
